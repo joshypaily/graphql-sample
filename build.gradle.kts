dfe53e8d287081ea5329ebd56fe068dfe2f9f934
joshypaily <joshypaily@gmail.com>
2024-07-20 20:20:37 +0530

    upgrade plugin.spring into 1.9.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.5"
 	kotlin("jvm") version "2.0.0"
-	kotlin("plugin.spring") version "1.9.24"
+	kotlin("plugin.spring") version "1.9.25"
 }
 
 group = "com.graphql"
